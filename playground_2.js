e9227460b579256ec2b49ba391ba81445f799989
dsans <sansdiego201@gmail.com>
2026-06-13 22:47:10 -0300

    I hear you, Odin
    playground 2

---
+++ playground_2.js
@@ -0,0 +1,8 @@
+function sidenav_open()
+{
+	if (document.getElementById("sidenav").style.width == "0px")
+		document.getElementById("sidenav").style.width = "250px"
+	else
+		document.getElementById("sidenav").style.width = "0px"
+	
+}
